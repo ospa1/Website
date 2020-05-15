3dfed3ded0f7af56fdd7948225f2f0457c5878c6
Oscar Palomino <oscarkil09@yahoo.com>
2020-05-15 12:10:13 -0500

    added buttons and functions

--- web.js
+++ web.js
@@ -1,3 +1,13 @@
+//display the date
 var date = new Date();
 var elem = document.getElementById('date');
 elem.textContent = date.toString();
+
+
+//changes the counter when the button is pressed
+var count = 0;
+function increaseCount(){
+	var counterText = document.getElementById('counter');
+	count++;
+	counterText.textContent = String(count);
+}
