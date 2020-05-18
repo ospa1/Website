29561615edb31deaf4e18d99a0be65639a861e60
ospa1 <oscarkil09@yahoo.com>
2020-05-17 19:14:14 -0500

    Create game01.js

---
+++ game01.js
@@ -0,0 +1,27 @@
+// get the buttons
+// get the marker
+
+// reset function
+function resetGame(){
+}
+
+// display the board, last 3 moves and forecast
+function displayTable(){
+}	
+
+// updates the table by adding 1 to the corresponding row and column in the 2D array.
+function updateTable(){
+}
+
+// compares the userInput and computerGuess values and move the marker,
+// tells the user who got a point, and updates the last three moves.
+function scoreUpdate()[
+}
+
+// Moves the '^' marker left or right depending on the result of scoreUpdate.
+function moveMarker()[
+}
+
+// Makes a prediction based on previous guesses.
+function computerPrediction(){
+}
