d59f974f8f76815d8246c50ed135a0bee450dcd5
ospa1 <oscarkil09@yahoo.com>
2020-05-18 21:05:02 -0500

    Update game01.js

--- game01.js
+++ game01.js
@@ -1,8 +1,27 @@
 // get the buttons
 // get the marker
 
+let moveCount = 0; // keeps track of the number of moves
+let tableArray = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}; // 2D array to store the user's input history.
+const possibleList = {"000","001","010","011","100","101","110","111"}; 
+let lastMove = 0;
+let lastThreeMoves = {0,0,0};
+
 // reset function
 function resetGame(){
+	
+	// reset table
+	tableArray = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+	
+	//resets the last three moves to 0.
+    lastThreeMoves[0] = 0;
+    lastThreeMoves[1] = 0;
+    lastThreeMoves[2] = 0;
+    
+    // resets the moveCount back to 0.
+    moveCount = 0;
+	
+	// TODO notify player
 }
 
 // display the board, last 3 moves and forecast
