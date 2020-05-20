862cef1ca67c8fd55480995199fcd4c84554b89a
ospa1 <oscarkil09@yahoo.com>
2020-05-19 19:29:32 -0500

    filled in some functions

--- game01.js
+++ game01.js
@@ -2,16 +2,17 @@
 // get the marker
 
 let moveCount = 0; // keeps track of the number of moves
-let tableArray = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}}; // 2D array to store the user's input history.
-const possibleList = {"000","001","010","011","100","101","110","111"}; 
+let tableArray = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]; // 2D array to store the user's input history.
+const possibleList = ["000","001","010","011","100","101","110","111"]; 
 let lastMove = 0;
-let lastThreeMoves = {0,0,0};
+let lastThreeMoves = [0,0,0];
+let forecast = 0;
 
 // reset function
 function resetGame(){
 	
 	// reset table
-	tableArray = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+	tableArray = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]];
 	
 	//resets the last three moves to 0.
     lastThreeMoves[0] = 0;
@@ -26,6 +27,8 @@ function resetGame(){
 
 // display the board, last 3 moves and forecast
 function displayTable(){
+	let disp = document.getElementById("display")
+	disp.textContent = tableArray;
 }	
 
 // updates the table by adding 1 to the corresponding row and column in the 2D array.
@@ -34,11 +37,11 @@ function updateTable(){
 
 // compares the userInput and computerGuess values and move the marker,
 // tells the user who got a point, and updates the last three moves.
-function scoreUpdate()[
+function score(num){
 }
 
 // Moves the '^' marker left or right depending on the result of scoreUpdate.
-function moveMarker()[
+function moveMarker(){
 }
 
 // Makes a prediction based on previous guesses.
